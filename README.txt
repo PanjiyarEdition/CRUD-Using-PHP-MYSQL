e15678662da86d46633c867acd5effa52572a5e8
PanjiyarEdition <jigarpanjiyar@gmail.com>
2023-09-07 19:01:50 +0530

    update all files

---
+++ README.txt
@@ -0,0 +1,6 @@
+Create Database name: user_db
+then run 
+
+login id
+Gmail: ram@gmail.com
+Password: 123
---
+++ user_db.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 07, 2023 at 03:16 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `user_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_form`
+--
+
+CREATE TABLE `user_form` (
+  `id` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `image` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_form`
+--
+
+INSERT INTO `user_form` (`id`, `name`, `email`, `password`, `image`) VALUES
+(1, 'ram sita', 'ram@gmail.com', '202cb962ac59075b964b07152d234b70', 'download.jpeg'),
+(15, 'rawan', 'rawan@gmail.com', '202cb962ac59075b964b07152d234b70', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user_form`
+--
+ALTER TABLE `user_form`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user_form`
+--
+ALTER TABLE `user_form`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
